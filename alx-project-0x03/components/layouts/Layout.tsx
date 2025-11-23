0343d3f6cea0813a3a9a4d8f575adf4705f53f55
veenzent <odumevincent19@gmail.com>
2025-11-23 20:51:04 +0100

    directory management

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,10 +1,6 @@
-import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-
-interface LayoutProps {
-  children: ReactNode;
-}
+import { LayoutProps } from "@/interface";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -2,10 +2,8 @@ import Button from "@/components/common/Button";
 import Header from "@/components/layouts/Header";
 import Footer from "@/components/layouts/Footer";
 import { useRouter } from "next/router";
+import { PageRouteProps } from "@/interface";
 
-interface PageRouteProps {
-  pageRoute: string
-}
 
 export default function Home() {
   const router = useRouter();
